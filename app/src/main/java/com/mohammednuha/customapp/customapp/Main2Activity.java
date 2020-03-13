8b14f0dd029d223b39c17a723294d6470a0d9e6f
Nuha Mohammed <nuha14m@gmail.com>
2020-03-13 13:31:32 -0400

    Lab06a Created layout for main screen and button to start game

--- app/src/main/java/com/mohammednuha/customapp/customapp/Main2Activity.java
+++ app/src/main/java/com/mohammednuha/customapp/customapp/Main2Activity.java
@@ -2,12 +2,37 @@ package com.mohammednuha.customapp.customapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
 
+    public Button btn1, btn2, btn3, btn4;
+    public int[] birds = new int [10];
+    int counter=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btn1= findViewById(R.id.btn1);
+        btn2= findViewById(R.id.btn2);
+        btn3= findViewById(R.id.btn3);
+        btn4= findViewById(R.id.btn4);
+
+    }
+
+    public void changeOptions(){
+        //btn1.setText();
+    }
+    public void changePic(){
+
+    }
+    @Override
+    public void onClick(View view) {
+        //change image and options
+     //   if(birds[])
+        //if wrong restart
+
     }
 }
